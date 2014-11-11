6c0e1c267240c41035ec645aace4c49f8bc0ef5f
Juhász Viktor <viktor.juhasz@balabit.com>
2014-11-11 11:57:59 +0100

    smake: add this module into smake framework
    Signed-off-by: Juhász Viktor <viktor.juhasz@balabit.com>

---
+++ smake/configure_opts.py
@@ -0,0 +1,1 @@
+default_opts="--enable-thread-safety --without-docdir --without-tcl --without-perl --without-python --without-krb5 --without-pam --without-ldap --without-bonjour --without-openssl --without-readline"
